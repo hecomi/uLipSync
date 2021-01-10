b7a46fe500944e2cbb03ee6ddf6d367365604bf2
hecomi <info@hecomi.com>
2021-01-10 23:59:28 +0900

    consider frame rate in smoothness calculations.

---
+++ Assets/uLipSync/Scripts/Core/Util.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace uLipSync
+{
+
+public static class Util
+{
+    public static void CalcNextValue(ref float current, float target, float smoothness)
+    {
+        int frameRate = Application.targetFrameRate;
+        if (frameRate < 0f) frameRate = 60;
+        float a = Mathf.Pow(1f - smoothness, frameRate / 60f);
+        a = Mathf.Clamp(a, 0f, 1f);
+        current = Mathf.Lerp(current, target, a);
+    }
+}
+
+}
