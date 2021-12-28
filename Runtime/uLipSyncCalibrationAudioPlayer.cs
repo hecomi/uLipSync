2b02d9e2e0861eeeeeb1185bec62711f6ae7e1bd
hecomi <info@hecomi.com>
2021-12-27 19:04:10 -0800

    update calibration player editor and imrpove unity chan profile.

--- Runtime/uLipSyncCalibrationAudioPlayer.cs
+++ Runtime/uLipSyncCalibrationAudioPlayer.cs
@@ -13,27 +13,13 @@ public class uLipSyncCalibrationAudioPlayer : MonoBehaviour
 
     AudioClip _tmpClip;
     float[] _data;
-    int _position = 0;
+    int _currentPos = 0;
     bool _audioReadCalled = false;
-    float _audioReadTime = 0f;
-    int _audioReadBasePos = 0;
     int _sampleRate = 0;
-    int _samples = 0;
+    int _sampleCount = 0;
     int _channels = 1;
-    int _crossFadeLen = 0;
-
-    public float position 
-    { 
-        get
-        {
-            if (_data == null || !_tmpClip) return 0f;
-            var dt = _audioReadCalled ? 0f : Time.time - _audioReadTime;
-            var pos = (float)_audioReadBasePos / _data.Length;
-            pos += dt / _tmpClip.length;
-            while (pos > 1f) pos -= 1f;
-            return pos;
-        }
-    }
+    int _crossFadeDataCount = 0;
+    int playDataSampleCount => _sampleCount - _crossFadeDataCount;
 
     public bool isPlaying
     {
@@ -56,12 +42,9 @@ public class uLipSyncCalibrationAudioPlayer : MonoBehaviour
 
     void Update()
     {
-        if (_audioReadCalled)
-        {
-            _audioReadCalled = false;
-            _audioReadTime = Time.time;
-            _sampleRate = AudioSettings.outputSampleRate;
-        }
+        if (!_audioReadCalled) return;
+
+        _sampleRate = AudioSettings.outputSampleRate;
     }
 
     public void Apply()
@@ -74,25 +57,24 @@ public class uLipSyncCalibrationAudioPlayer : MonoBehaviour
         var startPos = (int)(clip.samples * start);
         var endPos = (int)(clip.samples * end);
         var freq = clip.frequency;
-        _samples = endPos - startPos;
+        _sampleCount = endPos - startPos;
         _channels = clip.channels;
-        _crossFadeLen = (int)(_sampleRate * crossFadeDuration);
-        _crossFadeLen = Mathf.Min(_crossFadeLen, _samples / 2);
+        _crossFadeDataCount = (int)(_sampleRate * crossFadeDuration);
+        _crossFadeDataCount = Mathf.Min(_crossFadeDataCount, _sampleCount / 2 - 1);
 
-        _data = new float[_samples * _channels];
+        _data = new float[_sampleCount * _channels];
         clip.GetData(_data, startPos);
 
         var name = $"{clip.name}-{startPos}-{endPos}";
         _tmpClip = AudioClip.Create(
             name, 
-            _samples - _crossFadeLen, 
+            playDataSampleCount,
             _channels, 
             freq, 
             true, 
             OnAudioRead, 
             OnAudioSetPosition);
         source.clip = _tmpClip;
-
         source.loop = true;
         source.Play();
     }
@@ -100,36 +82,51 @@ public class uLipSyncCalibrationAudioPlayer : MonoBehaviour
     void OnAudioRead(float[] data)
     {
         _audioReadCalled = true;
-        _audioReadBasePos = _position;
 
         for (int i = 0; i < data.Length / _channels; ++i)
         {
             for (int ch = 0; ch < _channels; ++ch)
             {
-                if (_position < _crossFadeLen)
+                if (_currentPos < _crossFadeDataCount)
                 {
-                    float t = (float)_position / _crossFadeLen;
+                    float t = (float)_currentPos / _crossFadeDataCount;
                     float sin = Mathf.Sin(Mathf.PI * 0.5f * t);
                     float cos = Mathf.Cos(Mathf.PI * 0.5f * t);
-                    int indexS = _position;
-                    int indexE = _samples - (_crossFadeLen - _position);
+                    int indexS = _currentPos;
+                    int indexE = _sampleCount - (_crossFadeDataCount - _currentPos);
                     float dataS = _data[indexS * _channels + ch];
                     float dataE = _data[indexE * _channels + ch];
                     data[i] = dataS * sin + dataE * cos;
                 }
                 else
                 {
-                    data[i] = _data[_position * _channels + ch];
+                    data[i] = _data[_currentPos * _channels + ch];
                 }
             }
 
-            _position = (_position + 1) % (_samples - _crossFadeLen);
+            _currentPos = (_currentPos + 1) % playDataSampleCount;
         }
     }
 
     void OnAudioSetPosition(int newPosition)
     {
-        _position = newPosition;
+        _currentPos = newPosition;
+    }
+
+    public void Pause()
+    {
+        var source = GetComponent<AudioSource>();
+        if (!source) return;
+
+        source.Pause();
+    }
+
+    public void UnPause()
+    {
+        var source = GetComponent<AudioSource>();
+        if (!source) return;
+
+        source.UnPause();
     }
 }
 
