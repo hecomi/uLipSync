5eeb598cb849d6086c5eba302d6507dfb967a8e6
hecomi <info@hecomi.com>
2022-12-27 19:03:55 +0900

    add animator example.

--- Assets/uLipSync/Runtime/uLipSyncAnimator.cs
+++ Assets/uLipSync/Runtime/uLipSyncAnimator.cs
@@ -3,186 +3,151 @@ using System.Collections.Generic;
 
 namespace uLipSync
 {
-	[ExecuteAlways]
-	public class uLipSyncAnimator : MonoBehaviour
-	{
-		[System.Serializable]
-		public class AnimatorInfo
-		{
-			public string phoneme;
-			public int index = -1;
-			public float maxWeight = 1f;
 
-			public float weight { get; set; } = 0f;
-			public float weightVelocity { get; set; } = 0f;
-			public int nameHash;
-			public string name;
-		}
+[ExecuteAlways]
+public class uLipSyncAnimator : MonoBehaviour
+{
+    [System.Serializable]
+    public class AnimatorInfo
+    {
+        public string phoneme;
+        public string name;
+        public int nameHash;
+        public int index = -1;
+        public float maxWeight = 1f;
 
-		public UpdateMethod updateMethod = UpdateMethod.LateUpdate;
-		public Animator animator;
+        public float weight { get; set; } = 0f;
+        public float weightVelocity { get; set; } = 0f;
+    }
 
-		/// <summary>
-		/// Animator and animatorinfo for controlling the Animator parameters.
-		/// </summary>
-		public List<AnimatorInfo> parameters = new List<AnimatorInfo>();
-		public float minVolume = -2.5f;
-		public float maxVolume = -1.5f;
-		[Range(0f, 0.3f)] public float smoothness = 0.05f;
+    public UpdateMethod updateMethod = UpdateMethod.LateUpdate;
+    public Animator animator;
 
-		LipSyncInfo _info = new LipSyncInfo();
-		bool _lipSyncUpdated = false;
-		float _volume = 0f;
-		float _openCloseVelocity = 0f;
-		protected float volume => _volume;
+    public List<AnimatorInfo> parameters = new List<AnimatorInfo>();
+    public float minVolume = -2.5f;
+    public float maxVolume = -1.5f;
+    [Range(0f, 0.3f)] public float smoothness = 0.05f;
 
-#if UNITY_EDITOR
-		bool _isAnimationBaking = false;
-		float _animBakeDeltaTime = 1f / 60;
-#endif
+    LipSyncInfo _info = new LipSyncInfo();
+    bool _lipSyncUpdated = false;
+    float _volume = 0f;
+    float _openCloseVelocity = 0f;
+    protected float volume => _volume;
 
-		public void OnLipSyncUpdate(LipSyncInfo info)
-		{
-			_info = info;
-			_lipSyncUpdated = true;
-			if (updateMethod == UpdateMethod.LipSyncUpdateEvent)
-			{
-				UpdateVolume();
-				UpdateVowels();
-				_lipSyncUpdated = false;
-				OnApplyAnimator();
-			}
-		}
-		void Awake()
-		{
-			foreach (AnimatorInfo par in parameters)
-			{
-				par.nameHash = Animator.StringToHash(par.name);
-			}
-		}
+    void UpdateLipSync()
+    {
+        UpdateVolume();
+        UpdateVowels();
+        _lipSyncUpdated = false;
+    }
 
-		void Update()
-		{
-#if UNITY_EDITOR
-			if (_isAnimationBaking)
-				return;
-#endif
-			if (updateMethod != UpdateMethod.LipSyncUpdateEvent)
-			{
-				UpdateVolume();
-				UpdateVowels();
-				_lipSyncUpdated = false;
-			}
+    public void OnLipSyncUpdate(LipSyncInfo info)
+    {
+        _info = info;
+        _lipSyncUpdated = true;
+        if (updateMethod == UpdateMethod.LipSyncUpdateEvent)
+        {
+            UpdateLipSync();
+            OnApplyAnimator();
+        }
+    }
 
-			if (updateMethod == UpdateMethod.Update)
-			{
-				OnApplyAnimator();
-			}
-		}
+    void Update()
+    {
+        if (updateMethod != UpdateMethod.LipSyncUpdateEvent)
+        {
+            UpdateLipSync();
+        }
 
-		void LateUpdate()
-		{
-#if UNITY_EDITOR
-			if (_isAnimationBaking)
-				return;
-#endif
-			if (updateMethod == UpdateMethod.LateUpdate)
-			{
-				OnApplyAnimator();
-			}
+        if (updateMethod == UpdateMethod.Update)
+        {
+            OnApplyAnimator();
+        }
+    }
 
-		}
-		void FixedUpdate()
-		{
-#if UNITY_EDITOR
-			if (_isAnimationBaking)
-				return;
-#endif
-			if (updateMethod == UpdateMethod.FixedUpdate)
-			{
-				OnApplyAnimator();
-			}
-		}
+    void LateUpdate()
+    {
+        if (updateMethod == UpdateMethod.LateUpdate)
+        {
+            OnApplyAnimator();
+        }
+    }
 
-		float SmoothDamp(float value, float target, ref float velocity)
-		{
-#if UNITY_EDITOR
-			return Mathf.SmoothDamp(value, target, ref velocity, smoothness, Mathf.Infinity, _animBakeDeltaTime);
-#else
-        	return Mathf.SmoothDamp(value, target, ref velocity, smoothness);
-#endif
-		}
+    void FixedUpdate()
+    {
+        if (updateMethod == UpdateMethod.FixedUpdate)
+        {
+            OnApplyAnimator();
+        }
+    }
 
-		void UpdateVolume()
-		{
-			float normVol = 0f;
-			if (_lipSyncUpdated && _info.rawVolume > 0f)
-			{
-				normVol = Mathf.Log10(_info.rawVolume);
-				normVol = (normVol - minVolume) / Mathf.Max(maxVolume - minVolume, 1e-4f);
-				normVol = Mathf.Clamp(normVol, 0f, 1f);
-			}
-#if UNITY_EDITOR
-			_volume = SmoothDamp(_volume, normVol, ref _openCloseVelocity);
-#else
+    float SmoothDamp(float value, float target, ref float velocity)
+    {
+        return Mathf.SmoothDamp(value, target, ref velocity, smoothness);
+    }
+
+    void UpdateVolume()
+    {
+        float normVol = 0f;
+        if (_lipSyncUpdated && _info.rawVolume > 0f)
+        {
+            normVol = Mathf.Log10(_info.rawVolume);
+            normVol = (normVol - minVolume) / Mathf.Max(maxVolume - minVolume, 1e-4f);
+            normVol = Mathf.Clamp(normVol, 0f, 1f);
+        }
         _volume = SmoothDamp(_volume, normVol, ref _openCloseVelocity);
-#endif
-		}
+    }
 
-		void UpdateVowels()
-		{
-			float sum = 0f;
-			var ratios = _info.phonemeRatios;
+    void UpdateVowels()
+    {
+        float sum = 0f;
+        var ratios = _info.phonemeRatios;
 
-			foreach (var par in parameters)
-			{
-				float targetWeight = 0f;
-				// float targetWeight = (par.phoneme == phoneme) ? 1f : 0f;
-				if (ratios != null && !string.IsNullOrEmpty(par.phoneme))
-				{
-					ratios.TryGetValue(par.phoneme, out targetWeight);
-				}
-				float weightVel = par.weightVelocity;
-				par.weight = SmoothDamp(par.weight, targetWeight, ref weightVel);
-				par.weightVelocity = weightVel;
-				sum += par.weight;
-			}
+        foreach (var param in parameters)
+        {
+            float targetWeight = 0f;
+            if (ratios != null && !string.IsNullOrEmpty(param.phoneme))
+            {
+                ratios.TryGetValue(param.phoneme, out targetWeight);
+            }
+            float weightVel = param.weightVelocity;
+            param.weight = SmoothDamp(param.weight, targetWeight, ref weightVel);
+            param.weightVelocity = weightVel;
+            sum += param.weight;
+        }
 
-			foreach (var par in parameters)
-			{
-				par.weight = sum > 0f ? par.weight / sum : 0f;
-			}
-		}
+        foreach (var param in parameters)
+        {
+            param.weight = sum > 0f ? param.weight / sum : 0f;
+        }
+    }
 
-		public void ApplyAnimator()
-		{
-			if (updateMethod == UpdateMethod.External)
-			{
-				OnApplyAnimator();
-			}
-		}
+    public void ApplyAnimator()
+    {
+        if (updateMethod == UpdateMethod.External)
+        {
+            OnApplyAnimator();
+        }
+    }
 
-		protected virtual void OnApplyAnimator()
-		{
-			if (!animator)
-				return;
+    void OnApplyAnimator()
+    {
+        if (!animator) return;
 
-			// use hash
-			foreach (var par in parameters)
-			{
-				if (par.index < 0)
-					continue;
-				animator.SetFloat(par.nameHash, 0f);
-			}
+        foreach (var param in parameters)
+        {
+            if (param.index < 0) continue;
+            animator.SetFloat(param.nameHash, 0f);
+        }
 
-			foreach (var par in parameters)
-			{
-				if (par.index < 0)
-					continue;
-				float weight = animator.GetFloat(par.nameHash);
-				weight += par.weight * par.maxWeight * volume;
-				animator.SetFloat(par.nameHash, weight);
-			}
-		}
-	}
+        foreach (var param in parameters)
+        {
+            if (param.index < 0) continue;
+            float weight = animator.GetFloat(param.nameHash);
+            weight += param.weight * param.maxWeight * volume;
+            animator.SetFloat(param.nameHash, weight);
+        }
+    }
+}
+
 }
