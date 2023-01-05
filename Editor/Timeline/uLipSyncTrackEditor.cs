23560a95b5a674eb488b2f738161811a2badf0b1
hecomi <info@hecomi.com>
2023-01-05 12:33:40 +0900

    add timeline setup helper.

--- Editor/Timeline/uLipSyncTrackEditor.cs
+++ Editor/Timeline/uLipSyncTrackEditor.cs
@@ -10,13 +10,18 @@ public class uLipSyncTrackTrackEditor : TrackEditor
 {
     Texture2D _iconTexture;
 
+    public override void OnCreate(TrackAsset track, TrackAsset copiedFrom)
+    {
+        base.OnCreate(track, copiedFrom);
+        track.name = copiedFrom ? copiedFrom.name : "uLipSync Track";
+    }
+
     public override TrackDrawOptions GetTrackOptions(TrackAsset track, Object binding)
     {
         if (!_iconTexture)
         {
             _iconTexture = Resources.Load<Texture2D>("uLipSync-Icon");
         }
-        track.name = "uLipSync Track";
         var options = base.GetTrackOptions(track, binding);
         options.icon = _iconTexture;
         return options;
