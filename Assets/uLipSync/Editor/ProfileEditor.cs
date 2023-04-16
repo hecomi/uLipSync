9085d94ad4b65664f89cb1230d9af90e21998a97
hecomi <info@hecomi.com>
2023-04-16 17:47:32 +0900

    remove unused usings.

--- Assets/uLipSync/Editor/ProfileEditor.cs
+++ Assets/uLipSync/Editor/ProfileEditor.cs
@@ -3,10 +3,8 @@ using UnityEditor;
 using UnityEditorInternal;
 using System.IO;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using uLipSync.Debugging;
-using UnityEngine.UI;
 
 namespace uLipSync
 {
